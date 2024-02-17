44b2e3b5e94f36d01f830c9c9d72533182ad97d9
dsugiada <dsugiada@gmail.com>
2024-02-17 13:28:41 +1100

    Working buttons on Dental Note component

--- frontend/src/features/dentalNotes/DentalNotes.tsx
+++ frontend/src/features/dentalNotes/DentalNotes.tsx
@@ -1,7 +1,14 @@
-// src/features/dentalNotes/DentalNotes.tsx
+import './DentalNotes.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faFileAlt,
+  faHome,
+  faStar,
+  faThumbsUp,
+} from '@fortawesome/free-solid-svg-icons'
+import { useSelector } from 'react-redux'
 import React, { useState, useEffect } from 'react';
 import useSocket from '../../core/hooks/useSocket';
-import './DentalNotes.scss';
 
 const questions = [
   {
@@ -13,6 +20,7 @@ const questions = [
 ];
 
 const DentalNotes = () => {
+  const theme = useSelector((state: any) => state.home.theme)
   const [selectedOptions, setSelectedOptions] = useState<Record<number, string[]>>({});
   const { send, socket } = useSocket();
 
@@ -46,25 +54,97 @@ const DentalNotes = () => {
   };
 
   return (
-    <div className="dental-notes">
-      {questions.map((question) => (
-        <div key={question.id} className="question">
-          <div className="question-text">{question.text}</div>
-          <div className="options">
-            {question.options.map((option) => (
-              <button
-                key={option}
-                className={`option-button ${selectedOptions[question.id]?.includes(option) ? 'selected' : ''}`}
-                onClick={() => handleOptionSelect(question.id, option)}
-              >
-                {option}
-              </button>
-            ))}
-          </div>
+    <div
+      className={`home-container animate__animated animate__fadeIn animate__delay-1s ${theme}`}
+    >
+      <div className="content">
+        <div className="title">
+          <FontAwesomeIcon className="icon" icon={faHome} />
+          Welcome
         </div>
-      ))}
-    </div>
-  );
-};
 
-export default DentalNotes;
+
+        <div className="dental-notes">
+          {questions.map((question) => (
+            <div key={question.id} className="question">
+              <div className="question-text">{question.text}</div>
+              <div className="options">
+                {question.options.map((option) => (
+                  <button
+                  key={option}
+                  className={`option-button ${selectedOptions[question.id]?.includes(option) ? 'selected' : ''}`}
+                  onClick={() => handleOptionSelect(question.id, option)}
+                >
+                  {option}
+                </button>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+
+
+        <div className="text">
+          Thanks for using my boilerplate, find some details below to get
+          started.
+        </div>
+        <div className="text">
+          Like the project? Show some support at{' '}
+          <a
+            className="link"
+            target="_blank"
+            rel="noreferrer"
+            href="https://ko-fi.com/tamas0547"
+          >
+            ko-fi
+          </a>
+          . <FontAwesomeIcon className="icon" icon={faThumbsUp} />
+          <br />
+          Don't forget to give it a star on{' '}
+          <a
+            className="link"
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/tamasszoke/mern-seed"
+          >
+            GitHub
+          </a>
+          . <FontAwesomeIcon className="icon" icon={faStar} />
+        </div>
+        <div className="title">
+          <FontAwesomeIcon className="icon" icon={faFileAlt} />
+          Summary
+        </div>
+        <div className="text">
+          As you probably know, it uses Node, Mocha, MongoDB on the backend, and
+          React, Jest, Cypress on the frontend. Both sides are armed with
+          TypeScript and packed with Docker. Using JWT for authentication and
+          authorization.
+        </div>
+        <div className="title">
+          <FontAwesomeIcon className="icon" icon={faFileAlt} />
+          Documentation
+        </div>
+        <div className="text">
+          See the readme for the complete documentation. It also includes
+          development practices, guidelines, and conventions.{' '}
+          <a
+            className="link"
+            href="https://github.com/tamasszoke/mern-seed#readme"
+          >
+            https://github.com/tamasszoke/mern-seed#readme
+          </a>
+        </div>
+        <div className="title">
+          <FontAwesomeIcon className="icon" icon={faFileAlt} />
+          Credits
+        </div>
+        <div className="text">
+          <p>Made by Tamas Szoke @ 2022</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DentalNotes
---
+++ frontend/src/features/dentalNotes/index.ts
@@ -0,0 +1,5 @@
+import Home from './DentalNotes'
+import homeSlice from './DentalNotes.slice'
+
+export { homeSlice }
+export default Home
