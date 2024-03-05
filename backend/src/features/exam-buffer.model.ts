3c52e3ea8cf3e0798f9d21e293959171f33a19ce
dsugiada <dsugiada@gmail.com>
2024-03-05 21:13:07 +1100

    new schema for exam buffer added

--- backend/src/features/exam-buffer.model.ts
+++ backend/src/features/exam-buffer.model.ts
@@ -1,69 +1,51 @@
-'use strict'
+'use strict';
 
-import mongoose from 'mongoose'
-const { Schema } = mongoose
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
 
 /**
- * Create schema
+ * Define a schema for question selections within an examination.
+ * This subdocument will store question IDs and the selected options.
  */
-const schema: mongoose.Schema = new Schema({
-  email: {
+const selectionSchema = new Schema({
+  questionId: {
+    type: Schema.Types.ObjectId, // Assuming question IDs are MongoDB ObjectIds
+    required: true,
+  },
+  selectedOptions: [{
+    type: String, // Assuming options are identified by some string identifier
+    required: true,
+  }],
+}, { _id: false }); // Disable _id for subdocument
+
+/**
+ * Create schema for examinations.
+ * This document will store user examinations and their selections.
+ */
+const examinationSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId, // Reference to the User model
+    required: true,
+    ref: 'User',
+  },
+  email: { // Optional, if you want to quickly identify the user without populating the userId
     type: String,
     required: true,
-    unique: true,
     minlength: 5,
     maxlength: 100,
   },
-  name: {
-    type: String,
-    minlength: 5,
-    maxlength: 100,
-  },
-  password: {
-    type: String,
-    minlength: 5,
-    maxlength: 1000,
-  },
-  salt: {
-    type: String,
-    minlength: 5,
-    maxlength: 20,
-  },
-  active: {
-    type: Boolean,
-    default: false,
-  },
-  activationCode: {
-    type: String,
-    default: '',
-  },
-  recoveryCode: {
-    type: String,
-    default: '',
-  },
-  refreshToken: {
-    token: {
-      type: String,
-      minlength: 5,
-      maxlength: 1000,
-    },
-    expiration: {
-      type: String,
-      minlength: 5,
-      maxlength: 500,
-    },
-  },
+  selections: [selectionSchema], // Embed the selection schema
   created: {
     type: Date,
-    required: true,
+    default: Date.now,
   },
   updated: {
     type: Date,
-    required: true,
+    default: Date.now,
   },
-})
+});
 
 /**
  * Create a model using the schema
  */
-export default mongoose.model('user', schema, 'user')
+export default mongoose.model('Examination', examinationSchema, 'examinations');
